9133027434ddbfcbcd83643ea9a8eff63c880541
Paul C Pederson <paul.c.pederson@gmail.com>
2015-05-04 21:45:09 -0700

    evaluate view with data and apply edits to dom with dom-diff

---
+++ source/js/lib/render.js
@@ -0,0 +1,11 @@
+import DiffDOM from 'diff-dom'
+
+let dd = new DiffDOM()
+
+function render (node, view, data) {
+  let tmpNode = node.cloneNode(false)
+  tmpNode.innerHTML = view(data)
+  dd.apply(node, dd.diff(node, tmpNode))
+}
+
+export default render
