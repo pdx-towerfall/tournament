0c8a15df142814a27e349d0b6d88c0a845f92e11
Paul C Pederson <paul.c.pederson@gmail.com>
2015-05-06 14:53:16 -0700

    stub out schedule model

--- source/js/models/schedule.js
+++ source/js/models/schedule.js
@@ -1,7 +1,31 @@
-// Schedule Model
+// Save items to local storage
+// Surface add, remove, update, and delete
+import events from 'events'
+import assign from 'object-assign'
+import Store from '../lib/store.js'
+import toArray from '../lib/toArray.js'
+import toObject from '../lib/toObject.js'
+import schedule from '../services/schedule.js'
 
-/* Needs:
-  - create a new schedule with players (could call separate service)
-  - update player skulls in game (& add skulls to player)
-  - update winner for game (& add win to player)
-*/
+let store = Store('towerfall-test')
+
+function create () {
+  store(state => {
+    state.games = schedule(state.players)
+    return state
+  })
+}
+
+function update (game) {
+  store(state => {
+    assign(state.games[game.id], game)
+    return state
+  })
+}
+
+store.on('changed', state => {
+  events.emit('render', state)
+})
+
+events.on('schedule:create', create)
+events.on('schedule:update', update)
