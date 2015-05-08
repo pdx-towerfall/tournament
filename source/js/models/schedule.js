07be6d553eb0b71b68fd0f452a66ca62f7b884fb
Paul C Pederson <paul.c.pederson@gmail.com>
2015-05-07 17:43:30 -0700

    update player totals whenever games are updated

--- source/js/models/schedule.js
+++ source/js/models/schedule.js
@@ -9,8 +9,31 @@ import schedule from '../services/schedule.js'
 
 let store = Store('towerfall-test')
 
+// update wins and skulls of all players
+function calculatePlayerTotals (statePlayers, stateGames) {
+  let games = toArray(stateGames)
+  let players = toArray(statePlayers).map(p => {
+    return assign(p, {wins: 0, skulls: 0})
+  })
+  games.forEach(game => {
+    if (game.winner) {
+      statePlayers[game.winner].wins += 1
+    }
+    game.players.forEach(player => {
+      if (statePlayers[player.id]) {
+        statePlayers[player.id].skulls += player.skulls
+      }
+    })
+  })
+  statePlayers = toObject(players)
+}
+
 function create () {
   store(state => {
+    let players = toArray(state.players).map(p => {
+      return assign(p, {wins: 0, skulls: 0})
+    })
+    state.players = toObject(players)
     state.games = schedule(state.players)
     return state
   })
@@ -19,7 +42,22 @@ function create () {
 function update (game) {
   store(state => {
     assign(state.games[game.id], game)
-    console.log(state.games[game.id])
+    calculatePlayerTotals(state.players, state.games)
+    return state
+  })
+}
+
+
+function updateSkulls (game, data) {
+  console.log(data)
+  store(state => {
+    state.games[game].players.forEach((p, i) => {
+      if (p.id === data.id) {
+        state.games[game].players[i] = data
+      }
+    })
+    calculatePlayerTotals(state.players, state.games)
+    console.log(state.games[game])
     return state
   })
 }
@@ -30,3 +68,4 @@ store.on('changed', state => {
 
 events.on('schedule:create', create)
 events.on('schedule:update', update)
+events.on('schedule:updateSkulls', updateSkulls)
